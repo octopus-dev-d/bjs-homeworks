79685e52091272c4ab3973f9f353c9aa242b04ea
Octopus <octopus.dev@yandex.kz>
2021-01-19 10:27:23 +0600

    homeworks completed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,31 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
+
+	let averageRating = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return averageRating;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	
+	let message = `Привет, мир! Меня зовут ${userName}`;
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
