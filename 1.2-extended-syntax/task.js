3720f29c92c402a110b81023e3c2c306a81426bb
Octopus <octopus.dev@yandex.kz>
2021-01-20 14:50:35 +0600

    2 and tasks completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -19,11 +19,37 @@ function getResult(a,b,c){
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let sumResult = 0;
+    let averageMark = '';
+    const numberMarks = 5;
+    for (let i = 0; i < marks.length; i++) {
+        sumResult = sumResult + marks[i];
+        if (marks.length > numberMarks) {
+            console.log(marks.length);
+            marks.splise(numberMarks);
+        }
+    }
+
+    averageMark = sumResult / marks.length;
+    console.log(averageMark);
+    
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    
+    let currentDate = new Date();
+    console.log(currentDate);
+    let personAge = currentDate.getFullYear() - dateOfBirthday.getFullYear();
+    let result = '';
+
+    if (personAge >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+        return result;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+        return result;
+    }
+
+    return result;
 }
