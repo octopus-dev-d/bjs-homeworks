c5fe21c387acf7d99cc2a508d88f89376a14ea46
Octopus <octopus.dev@yandex.kz>
2021-01-20 11:48:11 +0600

    First task fixed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,8 +1,21 @@
 "use strict";
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+   
+    let d = (Math.pow(b, 2)) - 4 * a * c;
+    let result = [];
+
+        if (d < 0) {
+            result = [];
+            return result;
+        } else if (d == 0) {
+            result = [(-b + Math.sqrt(d)) / (2 * a)];
+            return result;
+        } else {
+            result = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+            return result;
+        }
+            return result;
 }
 
 function getAverageMark(marks){
