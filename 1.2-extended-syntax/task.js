687fc6aef3715c72619d2287d2d89988b2112567
Octopus <octopus.dev@yandex.kz>
2021-01-25 17:27:58 +0600

    tasks fixed2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,51 +3,44 @@
 function getResult(a,b,c){
    
     let d = (Math.pow(b, 2)) - 4 * a * c;
-    let result = [];
+    let result = '';
 
         if (d < 0) {
             result = [];
-            return result;
         } else if (d == 0) {
             result = [(-b + Math.sqrt(d)) / (2 * a)];
-            return result;
         } else {
             result = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
-            return result;
         }
             return result;
 }
 
 function getAverageMark(marks){
+
     let sumResult = 0;
     let averageMark = '';
     const numberMarks = 5;
+
     for (let i = 0; i < marks.length; i++) {
         sumResult = sumResult + marks[i];
+
         if (marks.length > numberMarks) {
-            console.log(marks.length);
-            marks.splise(numberMarks);
+            marks.splice(numberMarks);
         }
     }
-
-    averageMark = sumResult / marks.length;
-    console.log(averageMark);
-    
+    averageMark = sumResult / marks.length;  
     return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
     
-    let currentDate = new Date();
-    console.log(currentDate);
-    let personAge = currentDate.getFullYear() - dateOfBirthday.getFullYear();
+    let personAge = new Date().getFullYear() - dateOfBirthday.getFullYear();
     let result = '';
 
     if (personAge >= 18) {
         result = `Не желаете ли олд-фэшн, ${name}?`;
-        return result;
     } else {
         result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
-        return result;
     }
+    return result;
 }
