e828c4fc251ad303a983b906fd6091e3115bdd5d
Octopus <octopus.dev@yandex.kz>
2021-03-03 10:21:33 +0600

    task 2 and 3 completed

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,27 +1,85 @@
-function getResult(a, b, c) {
-    let roots;
-    
-    let d = Math.pow(b, 2) - 4*a*c; ;
-    let person = {
-        d ,
-        roots: []
-        };
-   
-   d = Math.pow(b, 2) - 4*a*c;
-    for (i=0; i<=1; i++)
-  {     
-     if (d < 0){
-         return person ;
-          }
-       if (d === 0) {
-       person.roots[i] = (-b + Math.sqrt(d)) / 2 * a
-        return person ;
+// Задание 1
+
+function getSolutions(a, b, c){
+    let D = Math.pow(b, 2) - (4 * a * c);
+ 
+    switch (true) {
+       case (D < 0):
+          return {D: D, roots: []}
+       case (D == 0):
+          let x1 = -b / (2 * a);
+          return {D: D, roots: [x1]}
+       case (D > 0):
+          let x2 = (-b + Math.sqrt(D)) / (2 * a);
+          let x3 = (-b - Math.sqrt(D)) / (2 * a);
+          return {D: D, roots: [x2, x3]}
+       }
+ }
+ 
+ function showSolutionsMessage(a, b, c) {
+    let result = getSolutions( a, b, c );
+ 
+    console.log(`«Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}»`);
+    console.log(`Значение дискриминанта: ${result.D}`);
+ 
+    switch (true) {
+       case (result.D < 0) :
+          console.log("Уравнение не имеет вещественных корней");
+          break;
+       case (result.D == 0) :
+          console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+          break;
+       case (result.D > 0) :
+          console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+          break;
+    }
+ }
+
+ // Задание 2
+
+ function getAverageMark(marks) {
+    if (marks.length === 0) return 0;
+    let sum = 0;
+  
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];
+    }
+    return sum / marks.length;
   }
-     if (d > 1) {
-         person.roots[i] = (-b + Math.sqrt(d)) / 2 * a;
-          person.roots[i+1] = (-b - Math.sqrt(d)) / 2 * a;
-    return person ;
-   
-     }
-}
-}
+  
+  
+  function getAverageScore(data) {
+    if (!data) return 0;
+  
+    const cloneData = { ...data };
+  
+    let sumMarks = 0;
+    let countMarks = 0;
+  
+    for (let key in cloneData) {
+      cloneData[key] = getAverageMark(cloneData[key]);
+  
+      sumMarks += cloneData[key];
+      countMarks++;
+    }
+  
+    sumMarks = sumMarks / countMarks;
+    if (countMarks == 0) sumMarks = 0;
+  
+    cloneData.average = sumMarks;
+  
+    return cloneData;
+  }
+  
+
+  // Задание 3
+
+  function getPersonData(secretData) { 
+    return { firstName: getDecodedValue(secretData.aaa), lastName: getDecodedValue(secretData.bbb) };
+  }
+  
+  function getDecodedValue(secret) {
+    if (secret == "1") return "Эмильо";
+    if (secret == "0") return "Родриго";
+    return "Broken data";
+  }
